79755b3a8e2217262d166675595ae0f1fd9857d4
parconl <parker.r2003@gmail.com>
2023-03-15 14:52:51 -0700

    implimented lat? function

--- testing.scm
+++ testing.scm
@@ -4,7 +4,15 @@
 
 #lang scheme
 
+; Definition of atom? given by book.
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
+; Definition of atom? given by book.
+(define (lat? l)
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
