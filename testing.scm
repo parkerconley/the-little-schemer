da805fd47f3c1f2145acccfd1290f5d75c8a4bc4
parconl <parker.r2003@gmail.com>
2023-03-16 18:21:27 -0700

    added member? function

--- testing.scm
+++ testing.scm
@@ -2,17 +2,22 @@
 ; Date: 03.14.2023
 ; Description: Following along with The Little Schemer.
 
-#lang scheme
+#lang racket
 
 ; Definition of atom? given by book.
-(define atom?
-  (lambda (x)
-    (and (not (pair? x)) (not (null? x)))))
-
+(define (atom? x)
+  ((and (not (pair? x)) (not (null? x)))))
+    
 ; Definition of atom? given by book.
 (define (lat? l)
-  (lambda (l)
-    (cond
+  (cond
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
-      (else #f))))
+      (else #f)))
+
+; Definition of member? given by book.
+(define (member? a lat)
+  (cond
+    ((null? lat) #f)
+    (else (or (eq? (car lat) a)
+              (member? a (cdr lat))))))
