9d07d6adbcd20c202e2bc4d0e673dc2167f6bc67
parconl <parker.r2003@gmail.com>
2023-03-16 17:10:38 -0700

    trying racket

---
+++ testing.rkt
@@ -0,0 +1,18 @@
+; Name: Parker Conley
+; Date: 03.14.2023
+; Description: Following along with The Little Schemer.
+
+#lang racket
+
+; Definition of atom? given by book.
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+; Definition of atom? given by book.
+(define (lat? l)
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
